db13af7d1936a74a1b539c1890732f5f11264d1f
OzgunEfe <ozgunefe.dev@gmail.com>
2024-08-03 21:18:28 +0100

    Card view and functions done.

---
+++ eShopApp/Components/ProductRow.swift
@@ -0,0 +1,18 @@
+//
+//  ProductRow.swift
+//  eShopApp
+//
+//  Created by Ozgun Efe on 03/08/2024.
+//
+
+import SwiftUI
+
+struct ProductRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductRow()
+}
