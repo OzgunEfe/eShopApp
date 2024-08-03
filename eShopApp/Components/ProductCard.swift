f20375a78bf39603b9c5fcca635dc5d58ac15690
OzgunEfe <ozgunefe.dev@gmail.com>
2024-08-03 20:15:11 +0100

    Product Card Design done.

---
+++ eShopApp/Components/ProductCard.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCard.swift
+//  eShopApp
+//
+//  Created by Ozgun Efe on 03/08/2024.
+//
+
+import SwiftUI
+
+struct ProductCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductCard()
+}
---
+++ eShopApp/Models/Product.swift
@@ -0,0 +1,8 @@
+//
+//  Product.swift
+//  eShopApp
+//
+//  Created by Ozgun Efe on 03/08/2024.
+//
+
+import Foundation
