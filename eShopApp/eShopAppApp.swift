dcaab44291386e521a95db1909639fe948480da3
OzgunEfe <ozgunefe.dev@gmail.com>
2024-08-03 19:44:25 +0100

    Initial Commit

---
+++ eShopApp/eShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  eShopAppApp.swift
+//  eShopApp
+//
+//  Created by Ozgun Efe on 03/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct eShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
