9b33cbbd869692c57ae74dc5192060fa067fd0bd
OzgunEfe <ozgunefe.dev@gmail.com>
2024-08-03 20:15:41 +0100

    Product Card Design done.

--- eShopApp/Components/ProductCard.swift
+++ eShopApp/Components/ProductCard.swift
@@ -8,11 +8,57 @@
 import SwiftUI
 
 struct ProductCard: View {
+    var product: Product
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .topTrailing) {
+            ZStack(alignment: .bottom) {
+                Image(product.image)
+                    .resizable()
+                    .cornerRadius(20)
+                    .frame(width: 180)
+                    .scaledToFit()
+                
+                VStack(alignment: .leading) {
+                    Text(product.name)
+                        .bold()
+                    
+                    Text("£\(product.price)")
+                        .font(.caption)
+                }
+                .padding()
+                .frame(width: 180, alignment: .leading)
+                .background(.ultraThinMaterial)
+                .clipShape(RoundedCorner(radius: 20, corners: [.bottomLeft, .bottomRight]))
+            }
+            .frame(width: 180, height: 250)
+            .shadow(radius: 3)
+            
+            Button {
+                print("Added to cart!")
+            } label: {
+                Image(systemName: "plus")
+                    .padding(10)
+                    .foregroundColor(.white)
+                    .background(.black)
+                    .cornerRadius(50)
+                    .padding(12)
+            }
+        }
+    }
+}
+
+// Custom shape to round only specified corners
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
     }
 }
 
 #Preview {
-    ProductCard()
+    ProductCard(product: productList[0])
 }
--- eShopApp/Models/Product.swift
+++ eShopApp/Models/Product.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct Product: Identifiable {
+    var id = UUID()
+    var name: String
+    var image: String
+    var price: Int
+}
+
+var productList = [Product(name: "Orange sweater", image: "sweater1", price: 55),
+                   Product(name: "Red wine sweater", image: "sweater2", price: 63),
+                   Product(name: "Sand sweater", image: "sweater3", price: 72),
+                   Product(name: "Sea sweater", image: "sweater4", price: 48),
+                   Product(name: "Cream sweater", image: "sweater5", price: 99),
+                   Product(name: "Beige sweater", image: "sweater6", price: 58),
+                   Product(name: "Grey sweater", image: "sweater7", price: 85),
+                   Product(name: "Mink sweater", image: "sweater8", price: 89)]
