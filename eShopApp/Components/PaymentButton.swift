3a8c6625640b96843ed357dde9acd5a3e4ac2214
OzgunEfe <ozgunefe.dev@gmail.com>
2024-08-04 10:44:39 +0100

    Apple Pay Button done

---
+++ eShopApp/Components/PaymentButton.swift
@@ -0,0 +1,18 @@
+//
+//  PaymentButton.swift
+//  eShopApp
+//
+//  Created by Ozgun Efe on 04/08/2024.
+//
+
+import SwiftUI
+
+struct PaymentButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaymentButton()
+}
